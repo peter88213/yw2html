7420c77cad9f16a4958a3a660b57f43bb5171d75
peter88213 <petert88213@gmail.com>
2020-07-18 23:11:46 +0200

    Update examples

--- bat/full_set.bat
+++ bat/full_set.bat
@@ -1,6 +1,8 @@
 ..\yw2html.pyw %1 -t template\manuscript -s _manuscript --silent
 ..\yw2html.pyw %1 -t template\chapters_synopsis -s _chapters --silent
 ..\yw2html.pyw %1 -t template\scenes_synopsis -s _scenes --silent
+..\yw2html.pyw %1 -t template\scenelist -s _scenelist --silent
 ..\yw2html.pyw %1 -t template\characters -s _characters --silent
 ..\yw2html.pyw %1 -t template\locations -s _locations --silent
 ..\yw2html.pyw %1 -t template\items -s _items --silent
+..\yw2html.pyw %1 -t template\notes -s _notes --silent
---
+++ bat/scene_ratings.bat
@@ -0,0 +1,2 @@
+python ..\yw2html.pyw %1 -t template\csv -s _ratings
+ren *_ratings.html *.csv
