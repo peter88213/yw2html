peter88213/yw2html

commit 2ed80892edaede984592264cdc885935ccfc0735

Prepare for PyWriter v8
Revise error handling:
- Throw exceptions instead of returning error messages.

src/yw2htmllib/html_templatefile_export.py
@@ -63,69 +63,69 @@ class HtmlTemplatefileExport(HtmlExport):
         templatePath = kwargs['template_path']
 
         # Project level.
-        __, content = read_html_file(f'{templatePath}/{self._HTML_HEADER}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._HTML_HEADER}{self.EXTENSION}')
         if content is not None:
             self._fileHeader = content
-        __, content = read_html_file(f'{templatePath}/{self._CHARACTER_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._CHARACTER_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._characterTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._LOCATION_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._LOCATION_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._locationTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._ITEM_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._ITEM_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._itemTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._HTML_FOOTER}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._HTML_FOOTER}{self.EXTENSION}')
         if content is not None:
             self._fileFooter = content
 
         # Chapter level.
-        __, content = read_html_file(f'{templatePath}/{self._PART_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._PART_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._partTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._CHAPTER_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._CHAPTER_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._chapterTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._CHAPTER_END_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._CHAPTER_END_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._chapterEndTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._UNUSED_CHAPTER_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._UNUSED_CHAPTER_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._unusedChapterTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._UNUSED_CHAPTER_END_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._UNUSED_CHAPTER_END_TEMPLATE}{self.EXTENSION}')
 
         if content is not None:
             self._unusedChapterEndTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._NOTES_CHAPTER_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._NOTES_CHAPTER_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._notesChapterTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._NOTES_CHAPTER_END_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._NOTES_CHAPTER_END_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._notesChapterEndTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._TODO_CHAPTER_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._TODO_CHAPTER_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._todoChapterTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._TODO_CHAPTER_END_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._TODO_CHAPTER_END_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._todoChapterEndTemplate = content
 
         # Scene level.
-        __, content = read_html_file(f'{templatePath}/{self._SCENE_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._SCENE_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._sceneTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._FIRST_SCENE_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._FIRST_SCENE_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._firstSceneTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._UNUSED_SCENE_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._UNUSED_SCENE_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._unusedSceneTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._NOTES_SCENE_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._NOTES_SCENE_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._notesSceneTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._TODO_SCENE_TEMPLATE}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._TODO_SCENE_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._todoSceneTemplate = content
-        __, content = read_html_file(f'{templatePath}/{self._SCENE_DIVIDER}{self.EXTENSION}')
+        content = read_html_file(f'{templatePath}/{self._SCENE_DIVIDER}{self.EXTENSION}')
         if content is not None:
             self._sceneDivider = content
 

.gitignore
@@ -1,3 +1,6 @@
 /test/yw7/
 /build/
 *.pyc
+*.bak
+/test/yw2html.py
+/test/test_yw2html.py

.project
@@ -3,6 +3,7 @@
 	<name>yw2html</name>
 	<comment></comment>
 	<projects>
+		<project>PyWriter</project>
 	</projects>
 	<buildSpec>
 		<buildCommand>

src/build_yw2html.py
@@ -20,10 +20,10 @@ TARGET_FILE = f'{BUILD}yw2html.py'
 
 
 def main():
-    # inliner.run(SOURCE_FILE, TARGET_FILE, 'yw2htmllib', '../src/', copyPyWriter=True)
-    # inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../../PyWriter/src/', copyPyWriter=True)
-    inliner.run(SOURCE_FILE, TARGET_FILE, 'yw2htmllib', '../src/')
-    inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../src/')
+    inliner.run(SOURCE_FILE, TARGET_FILE, 'yw2htmllib', '../src/', copyPyWriter=False)
+    inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../../PyWriter/src/', copyPyWriter=False)
+    # inliner.run(SOURCE_FILE, TARGET_FILE, 'yw2htmllib', '../src/')
+    # inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../src/')
     print('Done.')
 
 

src/pywriter/converter/export_source_factory.py
@@ -1,36 +1,0 @@
-"""Provide a factory class for a yWriter object to read.
-
-Copyright (c) 2022 Peter Triesberger
-For further information see https://github.com/peter88213/PyWriter
-Published under the MIT License (https://opensource.org/licenses/mit-license.php)
-"""
-import os
-from pywriter.pywriter_globals import *
-from pywriter.converter.file_factory import FileFactory
-
-
-class ExportSourceFactory(FileFactory):
-    """A factory class that instantiates a yWriter object to read.
-
-    Public methods:
-        make_file_objects(self, sourcePath, **kwargs) -- return conversion objects.
-    """
-
-    def make_file_objects(self, sourcePath, **kwargs):
-        """Instantiate a source object for conversion from a yWriter project.
-
-        Positional arguments:
-            sourcePath -- str: path to the source file to convert.
-
-        Return a tuple with three elements:
-        - A message beginning with the ERROR constant in case of error
-        - sourceFile: a YwFile subclass instance, or None in case of error
-        - targetFile: None
-        """
-        __, fileExtension = os.path.splitext(sourcePath)
-        for fileClass in self._fileClasses:
-            if fileClass.EXTENSION == fileExtension:
-                sourceFile = fileClass(sourcePath, **kwargs)
-                return 'Source object created.', sourceFile, None
-
-        return f'{ERROR}{_("File type is not supported")}: "{os.path.normpath(sourcePath)}".', None, None

src/pywriter/converter/export_target_factory.py
@@ -1,42 +1,0 @@
-"""Provide a factory class for a document object to write.
-
-Copyright (c) 2022 Peter Triesberger
-For further information see https://github.com/peter88213/PyWriter
-Published under the MIT License (https://opensource.org/licenses/mit-license.php)
-"""
-import os
-from pywriter.pywriter_globals import *
-from pywriter.converter.file_factory import FileFactory
-
-
-class ExportTargetFactory(FileFactory):
-    """A factory class that instantiates a document object to write.
-
-    Public methods:
-        make_file_objects(self, sourcePath, **kwargs) -- return conversion objects.
-    """
-
-    def make_file_objects(self, sourcePath, **kwargs):
-        """Instantiate a target object for conversion from a yWriter project.
-
-        Positional arguments:
-            sourcePath -- str: path to the source file to convert.
-
-        Required keyword arguments: 
-            suffix -- str: target file name suffix.
-
-        Return a tuple with three elements:
-        - A message beginning with the ERROR constant in case of error
-        - sourceFile: None
-        - targetFile: a FileExport subclass instance, or None in case of error 
-        """
-        fileName, __ = os.path.splitext(sourcePath)
-        suffix = kwargs['suffix']
-        for fileClass in self._fileClasses:
-            if fileClass.SUFFIX == suffix:
-                if suffix is None:
-                    suffix = ''
-                targetFile = fileClass(f'{fileName}{suffix}{fileClass.EXTENSION}', **kwargs)
-                return 'Target object created.', None, targetFile
-
-        return f'{ERROR}{_("Export type is not supported")}: "{suffix}".', None, None

src/pywriter/converter/file_factory.py
@@ -1,21 +1,0 @@
-"""Provide a base class for factories that instantiate conversion objects.
-
-Converter-specific file factories inherit from this class.
-
-Copyright (c) 2022 Peter Triesberger
-For further information see https://github.com/peter88213/PyWriter
-Published under the MIT License (https://opensource.org/licenses/mit-license.php)
-"""
-
-
-class FileFactory:
-    """Base class for conversion object factory classes.
-    """
-
-    def __init__(self, fileClasses=[]):
-        """Write the parameter to a "private" instance variable.
-
-        Optional arguments:
-            _fileClasses -- list of classes from which an instance can be returned.
-        """
-        self._fileClasses = fileClasses

src/pywriter/converter/import_source_factory.py
@@ -1,35 +1,0 @@
-"""Provide a factory class for a document object to read.
-
-Copyright (c) 2022 Peter Triesberger
-For further information see https://github.com/peter88213/PyWriter
-Published under the MIT License (https://opensource.org/licenses/mit-license.php)
-"""
-from pywriter.pywriter_globals import *
-from pywriter.converter.file_factory import FileFactory
-
-
-class ImportSourceFactory(FileFactory):
-    """A factory class that instantiates a documente object to read.
-
-    Public methods:
-        make_file_objects(self, sourcePath, **kwargs) -- return conversion objects.
-    """
-
-    def make_file_objects(self, sourcePath, **kwargs):
-        """Instantiate a source object for conversion to a yWriter project.       
-
-        Positional arguments:
-            sourcePath -- str: path to the source file to convert.
-
-        Return a tuple with three elements:
-        - A message beginning with the ERROR constant in case of error
-        - sourceFile: a Novel subclass instance, or None in case of error
-        - targetFile: None
-        """
-        for fileClass in self._fileClasses:
-            if fileClass.SUFFIX is not None:
-                if sourcePath.endswith(f'{fileClass.SUFFIX }{fileClass.EXTENSION}'):
-                    sourceFile = fileClass(sourcePath, **kwargs)
-                    return 'Source object created.', sourceFile, None
-
-        return f'{ERROR}{_("This document is not meant to be written back")}.', None, None

commit 28054a1a10d84e9d41a5ea6f5e8b600b5c5f47c6

Prepare v2.1.0 based on PyWriter v8.0.3
- Fix a regression from 2ed80892edaede984592264cdc885935ccfc0735 where
the program aborts if any template is not found.

src/yw2htmllib/html_templatefile_export.py
@@ -59,73 +59,80 @@ class HtmlTemplatefileExport(HtmlExport):
         
         Extends the superclass constructor.
         """
+
+        def load_template(filePath):
+            try:
+                return read_html_file(filePath)
+            except:
+                return None
+
         super().__init__(filePath)
         templatePath = kwargs['template_path']
 
         # Project level.
-        content = read_html_file(f'{templatePath}/{self._HTML_HEADER}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._HTML_HEADER}{self.EXTENSION}')
         if content is not None:
             self._fileHeader = content
-        content = read_html_file(f'{templatePath}/{self._CHARACTER_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._CHARACTER_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._characterTemplate = content
-        content = read_html_file(f'{templatePath}/{self._LOCATION_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._LOCATION_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._locationTemplate = content
-        content = read_html_file(f'{templatePath}/{self._ITEM_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._ITEM_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._itemTemplate = content
-        content = read_html_file(f'{templatePath}/{self._HTML_FOOTER}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._HTML_FOOTER}{self.EXTENSION}')
         if content is not None:
             self._fileFooter = content
 
         # Chapter level.
-        content = read_html_file(f'{templatePath}/{self._PART_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._PART_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._partTemplate = content
-        content = read_html_file(f'{templatePath}/{self._CHAPTER_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._CHAPTER_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._chapterTemplate = content
-        content = read_html_file(f'{templatePath}/{self._CHAPTER_END_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._CHAPTER_END_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._chapterEndTemplate = content
-        content = read_html_file(f'{templatePath}/{self._UNUSED_CHAPTER_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._UNUSED_CHAPTER_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._unusedChapterTemplate = content
-        content = read_html_file(f'{templatePath}/{self._UNUSED_CHAPTER_END_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._UNUSED_CHAPTER_END_TEMPLATE}{self.EXTENSION}')
 
         if content is not None:
             self._unusedChapterEndTemplate = content
-        content = read_html_file(f'{templatePath}/{self._NOTES_CHAPTER_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._NOTES_CHAPTER_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._notesChapterTemplate = content
-        content = read_html_file(f'{templatePath}/{self._NOTES_CHAPTER_END_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._NOTES_CHAPTER_END_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._notesChapterEndTemplate = content
-        content = read_html_file(f'{templatePath}/{self._TODO_CHAPTER_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._TODO_CHAPTER_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._todoChapterTemplate = content
-        content = read_html_file(f'{templatePath}/{self._TODO_CHAPTER_END_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._TODO_CHAPTER_END_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._todoChapterEndTemplate = content
 
         # Scene level.
-        content = read_html_file(f'{templatePath}/{self._SCENE_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._SCENE_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._sceneTemplate = content
-        content = read_html_file(f'{templatePath}/{self._FIRST_SCENE_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._FIRST_SCENE_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._firstSceneTemplate = content
-        content = read_html_file(f'{templatePath}/{self._UNUSED_SCENE_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._UNUSED_SCENE_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._unusedSceneTemplate = content
-        content = read_html_file(f'{templatePath}/{self._NOTES_SCENE_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._NOTES_SCENE_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._notesSceneTemplate = content
-        content = read_html_file(f'{templatePath}/{self._TODO_SCENE_TEMPLATE}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._TODO_SCENE_TEMPLATE}{self.EXTENSION}')
         if content is not None:
             self._todoSceneTemplate = content
-        content = read_html_file(f'{templatePath}/{self._SCENE_DIVIDER}{self.EXTENSION}')
+        content = load_template(f'{templatePath}/{self._SCENE_DIVIDER}{self.EXTENSION}')
         if content is not None:
             self._sceneDivider = content
 