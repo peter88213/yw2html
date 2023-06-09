83d4275846f68de6dd5705ed6e264a8b3fcac24e
peter88213 <peter.88213@t-online.de>
2023-06-09 12:09:23 +0200

    Update regression test

--- test/test_yw2html_.py
+++ test/test_yw2html_.py
@@ -16,13 +16,13 @@ from shutil import copyfile
 
 TEST_PATH = os.getcwd() + '/../test'
 TEST_DATA_PATH = TEST_PATH + '/data/'
-TEST_EXEC_PATH = TEST_PATH + '/'
+TEST_EXEC_PATH = TEST_PATH + '/tmp/'
 TEMPLATE_PATH = '../../template/'
 
 # To be placed in TEST_DATA_PATH:
 
 # Test data
-YW7 = 'normal.yw7'
+YW7_NORMAL = 'normal.yw7'
 YW7_INCL = 'with_templates.yw7'
 PAPERBACK = 'normal_paperback.html'
 SCENELIST = 'normal_scenelist.html'
@@ -42,7 +42,7 @@ def read_file(inputFile):
 
 def remove_all_testfiles():
     try:
-        os.remove(TEST_EXEC_PATH + YW7)
+        os.remove(TEST_EXEC_PATH + YW7_NORMAL)
     except:
         pass
     try:
@@ -67,38 +67,35 @@ class NormalOperation(unittest.TestCase):
     """Test case: Normal operation."""
 
     def setUp(self):
-        try:
-            os.mkdir(TEST_EXEC_PATH)
-        except:
-            pass
+        os.makedirs(TEST_EXEC_PATH, exist_ok=True)
         remove_all_testfiles()
-        copyfile(TEST_DATA_PATH + YW7, TEST_EXEC_PATH + YW7)
+        copyfile(TEST_DATA_PATH + YW7_NORMAL, TEST_EXEC_PATH + YW7_NORMAL)
 
     def test_scenelist(self):
         os.chdir(TEST_EXEC_PATH)
-        yw2html_.run(TEST_EXEC_PATH + YW7, TEMPLATE_PATH +
+        yw2html_.run(TEST_EXEC_PATH + YW7_NORMAL, TEMPLATE_PATH +
                      'scenelist', '_scenelist', True)
         self.assertEqual(read_file(TEST_EXEC_PATH + SCENELIST),
                          read_file(TEST_DATA_PATH + SCENELIST))
 
     def test_script(self):
         os.chdir(TEST_EXEC_PATH)
-        yw2html_.run(TEST_EXEC_PATH + YW7, TEMPLATE_PATH +
+        yw2html_.run(TEST_EXEC_PATH + YW7_NORMAL, TEMPLATE_PATH +
                      'manuscript', '_manuscript', True)
         self.assertEqual(read_file(TEST_EXEC_PATH + SCRIPT),
                          read_file(TEST_DATA_PATH + SCRIPT))
 
     def test_characters(self):
         os.chdir(TEST_EXEC_PATH)
-        yw2html_.run(TEST_EXEC_PATH + YW7, TEMPLATE_PATH +
+        yw2html_.run(TEST_EXEC_PATH + YW7_NORMAL, TEMPLATE_PATH +
                      'characters', '_characters', True)
         self.assertEqual(read_file(TEST_EXEC_PATH + CHARAS),
                          read_file(TEST_DATA_PATH + CHARAS))
 
     def test_templates_included(self):
         os.chdir(TEST_EXEC_PATH)
-        copyfile(TEST_DATA_PATH + YW7_INCL, TEST_EXEC_PATH + YW7)
-        yw2html_.run(TEST_EXEC_PATH + YW7, '', '_paperback', True)
+        copyfile(TEST_DATA_PATH + YW7_INCL, TEST_EXEC_PATH + YW7_NORMAL)
+        yw2html_.run(TEST_EXEC_PATH + YW7_NORMAL, '', '_paperback', True)
         self.assertEqual(read_file(TEST_EXEC_PATH + PAPERBACK),
                          read_file(TEST_DATA_PATH + PAPERBACK))
 
